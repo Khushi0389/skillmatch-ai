8f7fde305e0738772b40699b4aba66a37c8dc0fd
Khushi <ag.khushigupta@gmail.com>
2025-06-22 19:05:53 +0530

    Fix: Add spaCy model download in start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,1 +1,7 @@
-uvicorn app.main:app --host=0.0.0.0 --port=8000
+#!/bin/bash
+
+# Install Spacy language model
+python -m spacy download en_core_web_sm
+
+# Start the FastAPI server
+uvicorn app.main:app --host 0.0.0.0 --port 8000
